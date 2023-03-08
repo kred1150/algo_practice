e5f0018c77d00e9affb3ba68756c97f82493c85c
Kevin Redmond <kred1150@gmail.com>
2023-03-07 23:23:59 -0600

    Added the solution for the order the whole menu excersice

---
+++ basic_hash/order_menu.rb
@@ -0,0 +1,17 @@
+# Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
+
+# Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+
+# Output: 14
+
+# Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+def total_menu(hash)
+  total_cost = 0
+  hash.each do |item, price|
+    total_cost = total_cost + price.to_i
+  end
+  return total_cost
+end
+
+p total_menu({ "hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2 })
