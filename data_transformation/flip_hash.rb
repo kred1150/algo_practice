200e14543cd2f0d3a3e9ebdd9516c8df0f78e473
Kevin Redmond <kred1150@gmail.com>
2023-03-13 22:02:23 -0500

    Added the solution for the flip hash excercise

---
+++ data_transformation/flip_hash.rb
@@ -0,0 +1,14 @@
+# Given a hash, create a new hash that has the keys and values switched.
+
+# Input: {"a" => 1, "b" => 2, "c" => 3}
+# Output: {1 => "a", 2 => "b", 3 => "c"}
+
+def flip(hash)
+  flipped = {}
+  hash.each do |key, value|
+    flipped[value] = key
+  end
+  return flipped
+end
+
+p flip({ "a" => 1, "b" => 2, "c" => 3 })
